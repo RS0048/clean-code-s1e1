4e7e333cbea3939daf452159de6f401f8aecbe60
RS0048 <sokolovrmn@gmail.com>
2024-01-09 00:05:03 +0300

    fix(basic-3.1, 3.2): fixed name for BEM

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("newTask");//Add a new task.
+var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("workWithTasksToDo");//ul of #workWithTasksToDo
-var completedTasksHolder=document.getElementById("workWithCompletedTasks");//workWithCompletedTasks
+var incompleteTaskHolder=document.getElementById("work-with-tasks-to-do");//ul of #work-with-tasks-to-do
+var completedTasksHolder=document.getElementById("work-with-completed-tasks");//work-with-completed-tasks
 
 
 //New task list item
@@ -33,12 +33,12 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className="task";
+    label.className="task-item__task";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task-item__task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
@@ -61,7 +61,7 @@ var createNewTaskElement=function(taskString){
 
 var addTask=function(){
     console.log("Add Task...");
-    //Create a new list item with the text from the #newTask:
+    //Create a new list item with the text from the #new-task:
     if (!taskInput.value) return;
     var listItem=createNewTaskElement(taskInput.value);
 
@@ -85,11 +85,11 @@ var editTask=function(){
     var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit_modificed");
-    //If class of the parent is .edit_modificed
+    var containsClass=listItem.classList.contains("task-item_active");
+    //If class of the parent is .task-item_active
     if(containsClass){
 
-        //switch to .edit_modificed
+        //switch to .task-item_active
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +98,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit_modificed on the parent.
-    listItem.classList.toggle("edit_modificed");
+    //toggle .task-item_active on the parent.
+    listItem.classList.toggle("task-item_active");
 };
 
 
@@ -119,7 +119,7 @@ var deleteTask=function(){
 var taskCompleted=function(){
     console.log("Complete Task...");
 
-    //Append the task list item to the #workWithCompletedTasks
+    //Append the task list item to the #work-with-completed-tasks
     var listItem=this.parentNode;
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
@@ -131,7 +131,7 @@ var taskIncomplete=function(){
     console.log("Incomplete Task...");
 //Mark task as incomplete.
     //When the checkbox is unchecked
-    //Append the task list item to the #workWithTasksToDo.
+    //Append the task list item to the #work-with-tasks-to-do.
     var listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
